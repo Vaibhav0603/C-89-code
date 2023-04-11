4c618e64b08bd5a38ecde803128ca84d5fd20785
Vaibhav0603 <99812391+Vaibhav0603@users.noreply.github.com>
2023-04-11 19:08:55 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAhqGFJBYuF-M-qxQh59gheNnzAKfnmhlY",
+  authDomain: "storytelling-app-66f07.firebaseapp.com",
+  databaseURL: "https://storytelling-app-66f07-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-66f07",
+  storageBucket: "storytelling-app-66f07.appspot.com",
+  messagingSenderId: "969437790671",
+  appId: "1:969437790671:web:5b678eb0ba43fd20d3d2ba"
+};
